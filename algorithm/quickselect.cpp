ad8da4ee676c3321ba578fbfc031df051f065b7a
연주안 <yeonjuan93@naver.com>
2018-04-05 13:16:05 +0900

    error solv

---
+++ algorithm/quickselect.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int partition(int *arr, int left, int right){
+	int p = arr[right], i = left;
+	for( int j=left; j<= right-1; j++ ){
+		if( arr[j] <= p ){
+			swap(arr[i],arr[j]);
+			i++;
+		}
+	}
+	swap(arr[i],arr[right]);
+	return i;
+}
+
+int quickSelect(int *arr , int left, int right, int k)
+{	
+	if( k>0 && k <= right - left +1 ){
+		int part = partition( arr, left, right );
+
+		if(	part - left == k-1 )	return arr[part];
+		if( part - left > k-1 )		return quickSelect( arr, left, part-1, k );
+		return quickSelect( arr, part + 1, right, k -part +left -1 );
+	}
+	return INT_MAX;
+}
+
+int main(){
+	int N;
+	cin>>N;
+
+	int arr[N];
+	for( int i=0; i<N; i++ ){
+		cin>> arr[i];
+	}
+
+	int K;
+	cin>>K;
+	cout<< quickSelect( arr, 0, N-1, K );
+
+	return 0;
+}
+	
+
