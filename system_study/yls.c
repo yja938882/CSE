cb2f77174291f5bb7c7db64e51232acc2da81dc4
연주안 <yeonjuan93@naver.com>
2018-04-11 23:44:43 +0900

    linked list

--- system_study/yls.c
+++ system_study/yls.c
@@ -2,10 +2,36 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <dirent.h>
-#define DIR_COLOR 31 	// Red
+#include <string.h>
+#define DIR_COLOR 36 	// Cyan
 #define REG_COLOR 33 	// Yellow
 #define LNK_COLOR 36	// Cyan
-int getColor( char type ){
+
+#define LINE_MAX 8
+
+struct finfo{
+	char *name;
+	struct stat *fstat;
+	struct finfo* next;
+};
+
+void *chk_malloc( ssize_t );
+char *chk_strdup( char*);
+int get_color( char );
+struct finfo* read_dir( char *);
+void print_dir( struct finfo* );
+
+int main( int argc, char *argv[] ){
+	struct finfo * f;
+	if( argc == 1 )
+		f =read_dir("./");
+	if( argc == 2 )
+		f = read_dir(argv[1]);
+	print_dir(f);	
+	return 0;
+}
+
+int get_color( char type ){
 	switch( type ){
 		case DT_DIR :	return DIR_COLOR;
 		case DT_REG :	return REG_COLOR;
@@ -14,26 +40,61 @@ int getColor( char type ){
 	return 0;
 }
 
-void print_dir( char* dir_name ){
+struct finfo* create_finfo(struct dirent* dir_entry){
+	struct finfo* f = (struct finfo*) chk_malloc( sizeof( struct finfo) );
+	f->name = chk_strdup( dir_entry->d_name );
+	f->fstat = NULL;
+	f->next = NULL;
+	return f;
+}
+
+struct finfo* read_dir( char* dir_name){
+	struct finfo* front_f;
+	struct finfo* f;
 	DIR *dir_ptr;
 	struct dirent *dir_entry;
 
 	if( !(dir_ptr = opendir( dir_name )) ){
-		fprintf(stderr," cannot open %s\n",dir_name);
-		exit(-1);
+		fprintf( stderr, "cannot open %s\n", dir_name );
+		exit( -1 ); 
 	}
 
-	while( (dir_entry = readdir( dir_ptr)) ){
-		int color = getColor( dir_entry->d_type );
-		printf("\033[0;%dm %s\t",color, dir_entry -> d_name );
+	dir_entry = readdir( dir_ptr );
+	front_f = (struct finfo *)chk_malloc( sizeof( struct finfo ));
+	front_f->name = chk_strdup( dir_entry->d_name );
+	front_f->fstat = NULL;
+	front_f->next = NULL;
+	f = front_f;
+	
+	while( (dir_entry = readdir( dir_ptr )) ){
+		f->next = create_finfo( dir_entry);
+		f = f->next;
 	}
 
-	closedir( dir_ptr );
-	printf("\n");
+	return front_f; 
 }
 
-int main(int argc , char *argv[] ){
+char* chk_strdup( char* str ){
+	char* ptr = (char*)chk_malloc( strlen(str)+1 );
+	strcpy(ptr, str);
+	return ptr;
+}
 
-	print_dir("./");
-	return 0;
+void *chk_malloc( ssize_t size ){
+	void* ptr = malloc( size );
+	if( !ptr ){	
+		fprintf(stderr,"Fatal : failed to allocate memory.\n");
+		abort();
+	}
+	return ptr;
+}
+
+void print_dir( struct finfo* front ){
+	struct finfo* f = front;
+
+	while(f){
+		printf("%s\t",f->name);
+		f = f->next;
+	}
+	printf("\n");
 }
