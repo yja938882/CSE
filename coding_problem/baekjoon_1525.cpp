723b3245f82c97c46d0d3d88194e99f13cf8de3d
연주안 <yeonjuan93@naver.com>
2018-04-14 21:24:09 +0900

    1525 번 퍼즐

--- coding_problem/baekjoon_1525.cpp
+++ coding_problem/baekjoon_1525.cpp
@@ -2,7 +2,7 @@
 #include <queue>
 using namespace std;
 
-bool isCorrect( int y , int E, int S, int M){
+bool isCorrect( int y , int E, int S, int M ){
 	int ye = ( (y % 15) == 0 ) ? 15 : (y % 15);
 	int ys = ( (y % 28) == 0 ) ? 28 : (y % 28);
 	int ym = ( (y % 19) == 0 ) ? 19 : (y % 19);
@@ -13,9 +13,9 @@ int main(void){
 	int E, S, M;
 	cin >> E >>S >> M;
 
-	int y =1;
+	int y = 1;
 	while(1){
-		if(isCorrect(y , E, S, M)){
+		if( isCorrect(y , E, S, M) ){
 			cout << y<< endl;
 			break;
 		}
